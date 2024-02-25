1c13a86606c8c725959e92634a2eb0f3808c4bac
jant7070 <josemorillo702@gmail.com>
2024-02-25 17:13:22 -0400

    Arreglado el burger menu

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -12,10 +12,10 @@ export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-expand-md navbar-light bg-transparent">
 			<div className="container-fluid">
-				<img className="navbar-brand" src={logo} />
 				<button className="navbar-toggler text-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
 					<FontAwesomeIcon icon={faBars} size="lg" style={{ color: "#ffffff", }} />
 				</button>
+				<img className="navbar-brand" src={logo} />
 				<div className="collapse navbar-collapse" id="navbarTogglerDemo01">
 					<ul className="navbar-nav ms-auto mb-2 mb-lg-0">
 						<li className="nav-item">
@@ -25,7 +25,7 @@ export const Navbar = () => {
 							<Link className="nav-link" to="/" >Episodios</Link>
 						</li>
 						<li className="nav-item">
-							<Link className="nav-link" to="/" >Contacto</Link>
+							<Link className="nav-link" to="/contacto" >Contacto</Link>
 						</li>
 						<li className="nav-item">
 							<a href="https://www.tiktok.com/@epachams.podcast?_t=8kBJ6wGaeFa&_r=1">
