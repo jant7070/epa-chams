485b3beff2da824f96a83e37e764d0248011c050
jant7070 <josemorillo702@gmail.com>
2024-02-25 15:39:40 -0400

    Landing page done

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -6,13 +6,14 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTiktok } from "@fortawesome/free-brands-svg-icons";
 import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 import { faYoutube } from "@fortawesome/free-brands-svg-icons";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-expand-md navbar-light bg-transparent mb-3">
+		<nav className="navbar navbar-expand-md navbar-light bg-transparent">
 			<div className="container-fluid">
-				<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-					<span className="navbar-toggler-icon"></span>
+				<button className="navbar-toggler text-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+					<FontAwesomeIcon icon={faBars} size="lg" style={{ color: "#ffffff", }} />
 				</button>
 				<div className="collapse navbar-collapse" id="navbarTogglerDemo01">
 					<img className="navbar-brand" src={logo} />
@@ -27,9 +28,15 @@ export const Navbar = () => {
 							<Link className="nav-link" to="/" >Contacto</Link>
 						</li>
 						<li className="nav-item">
-							<FontAwesomeIcon className="mt-2 me-3 ms-2" icon={faTiktok} size="lg" />
-							<FontAwesomeIcon className="mt-2 me-3" icon={faYoutube} style={{ color: "red" }} size="lg" />
-							<FontAwesomeIcon className="instagram mt-2 me-3" icon={faInstagram} size="lg" />
+							<a href="https://www.tiktok.com/@epachams.podcast?_t=8kBJ6wGaeFa&_r=1">
+								<FontAwesomeIcon className="mt-2 me-3 ms-2" icon={faTiktok} style={{ color: "black" }} size="lg" />
+							</a>
+							<a href="https://youtube.com/@Epachamas?si=e9hDIpw0rDCwIj5O">
+								<FontAwesomeIcon className="mt-2 me-3" icon={faYoutube} style={{ color: "red" }} size="lg" />
+							</a>
+							<a href="https://www.instagram.com/epachamas?igsh=MW9uZTNxNzA0eXh2cw==">
+								<FontAwesomeIcon className="instagram mt-2 me-3" icon={faInstagram} size="lg" />
+							</a>
 						</li>
 					</ul>
 				</div>
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,21 @@
 import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import splash from "../../img/splash-Photoroom.png"
+import woman from "../../img/woman.png"
+
 import "../../styles/home.css";
+import "animate.css"
 
 export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-danger">
-			If you see this green button, bootstrap is working
-		</a>
+	<div className="landing-page d-flex">
+		<div className="text">
+			<h1 className="animate__animated animate__zoomIn ps-4 text-white">
+				El primer podcast, en Venezuela, que empodera a la juventud con
+				conocimientos financieros.
+			</h1>
+			<button className="btn btn-primary m-3">Episodios</button>
+			<button className="btn btn-primary m-3">Contacto</button>
+		</div>
+		<img className="img-fluid splash" src={splash} />
+		<img className="img-fluid woman" src={woman} />
 	</div>
 );
